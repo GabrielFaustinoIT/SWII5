8e9f36145a27d4ad75a365dcce33c1f5071683c3
Gabriel Faustino <90875131+GabrielFaustinoIT@users.noreply.github.com>
2023-05-02 18:41:24 -0300

    finalizado

---
+++ SWII5-TP02/src/RemoveProdutoServlet.java
@@ -0,0 +1,26 @@
+
+// Aluno
+// 
+// GABRIEL FAUSTINO CB3005399
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/removeProduto")
+public class RemoveProdutoServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DB banco = new DB();
+		String nome = request.getParameter("nome");
+					
+		banco.removeProduto(nome);
+		response.sendRedirect("listaProdutos");
+	}
+
+}
