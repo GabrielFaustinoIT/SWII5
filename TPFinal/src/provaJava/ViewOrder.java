bf925fa67c3feba5555084bae3fa4283049e4d53
Gabriel Faustino <90875131+GabrielFaustinoIT@users.noreply.github.com>
2023-06-13 13:00:46 -0300

    ViewOrder.java

--- TPFinal/src/provaJava/ViewOrder.java
+++ TPFinal/src/provaJava/ViewOrder.java
@@ -22,7 +22,7 @@ public class ViewOrder extends HttpServlet {
         List<order> list = generalDAO.getAllOrders();
         
         out.print("<table border='1' width='100%'");
-        out.print("<tr><th>Núm. Pedido</th><th>Valor do Pedido</th><th>Data da Compra</th><th>Cliente</th><th>Vendedor</th><th>Editar</th><th>Excluir</th></tr>");
+        out.print("<tr><th>NÃºm. Pedido</th><th>Valor do Pedido</th><th>Data da Compra</th><th>Cliente</th><th>Vendedor</th><th>Editar</th><th>Excluir</th></tr>");
         for(order e:list)
         {
             out.print("<tr><td>" + e.getOrdNo() + "</td><td>" + e.getPurchAmt() + "</td><td> "+e.getOrdDate()+" </td><td> "+e.getCustomerId()+" </td><td> "+e.getSalesmanId()+" </td><td><a href = 'EditOrder?ord_no="+e.getOrdNo()+"'> Editar </a></td><td><a href = 'DeleteOrder?ord_no="+e.getOrdNo()+"'> Excluir </a></td></tr> ");
@@ -34,5 +34,4 @@ public class ViewOrder extends HttpServlet {
 }
 
 
-//Douglas willian CB3005348
-//João Vitor Gino CB3005488
+//Gabriel Faustino CB3005399
