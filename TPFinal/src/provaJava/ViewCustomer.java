aae53a3ceb9081c1b7212f4f640478699bbe6e69
Gabriel Faustino <90875131+GabrielFaustinoIT@users.noreply.github.com>
2023-06-13 13:00:31 -0300

    ViewCustomer.java

--- TPFinal/src/provaJava/ViewCustomer.java
+++ TPFinal/src/provaJava/ViewCustomer.java
@@ -22,7 +22,7 @@ public class ViewCustomer extends HttpServlet {
         List<customer> list = generalDAO.getAllCustomers();
         
         out.print("<table border='1' width='100%'");
-        out.print("<tr><th>Id</th><th>Nome</th><th>Cidade</th><th>Avaliação</th><th>Vendedor</th><th>Editar</th><th>Excluir</th></tr>");
+        out.print("<tr><th>Id</th><th>Nome</th><th>Cidade</th><th>AvaliaÃ§Ã£o</th><th>Vendedor</th><th>Editar</th><th>Excluir</th></tr>");
         for(customer e:list)
         {
             out.print("<tr><td>" + e.getId() + "</td><td>" + e.getCustName() + "</td><td> "+e.getCity()+" </td><td> "+e.getGrade()+" </td><td> "+e.getSalesmanId()+" </td><td><a href = 'EditCustomer?customer_id="+e.getId()+"'> Editar </a></td><td><a href = 'DeleteCustomer?customer_id="+e.getId()+"'> Excluir </a></td></tr> ");
@@ -34,5 +34,4 @@ public class ViewCustomer extends HttpServlet {
 }
 
 
-//Douglas willian CB3005348
-//João Vitor Gino CB3005488
+//Gabriel Faustino CB3005399
